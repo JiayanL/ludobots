119805690220643cf037d533f6a0c17a5bb2d8ab
jiayan <jiayan@findourview.com>
2023-01-08 23:20:04 -0600

    comments

--- simulate.py
+++ simulate.py
@@ -1,6 +1,11 @@
 import pybullet as p
 import time
 
+"""
+Controlling Virtual Camera
+- Hold down control and click and drag
+"""
+
 physicsClient = p.connect(p.GUI)
 for i in range(1000):
     # steps inside the physics world for a small amount
