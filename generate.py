394724355ad1c6569a8262700760f899e6964447
jiayan <jiayan@findourview.com>
2023-01-09 00:02:52 -0600

    tower of 5

--- generate.py
+++ generate.py
@@ -2,11 +2,31 @@ import pyrosim.pyrosim as pyrosim
 
 # use pyrosim to generate a link
 # tells pyrosim the name of the file where information about the world should be stored
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
 # store in a world.sdf
 # stores box with parameters
-pyrosim.Send_Cube(name="box", pos=[0, 0, 0.5], size=[1, 1, 1])
+length, width, height = 1, 1, 1
+x, y, z = 0, 0, .5
+
+# 5 rows
+for k in range(5):
+    # 5 columns
+    for j in range(5):
+        # generate a tower
+        for i in range(10):
+            pyrosim.Send_Cube(name="Box" + str(i),
+                              pos=[x+j, y+k, z], size=[length, width, height])
+            # adjust height
+            z += height
+
+            # adjust block size
+            height *= .9
+            width *= .9
+            length *= .9
+        z = .5
+        length, width, height = 1, 1, 1
+
 # closes the file
 pyrosim.End()
 # read it in and simulate it
--- simulate.py
+++ simulate.py
@@ -1,4 +1,5 @@
 import pybullet as p
+import pybullet_data
 import time
 
 """
@@ -7,8 +8,19 @@ Controlling Virtual Camera
 """
 
 physicsClient = p.connect(p.GUI)
+
+# loads files like plane.urdf
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+# set gravity
+p.setGravity(0, 0, -9.8)
+# add a floor
+planeID = p.loadURDF("plane.urdf")
 # tells pybullet to read in the world described in box.sdf
-p.loadSDF("box.sdf")
+p.loadSDF("boxes.sdf")
+
+
+# run the simulation for 1000 steps
 for i in range(1000):
     # steps inside the physics world for a small amount
     p.stepSimulation()
