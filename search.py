68d25e6c752ea0bf86619c9aca206f18163b167c
jiayan <jiayan@findourview.com>
2023-03-14 10:46:43 -0700

    ran experiments

--- search.py
+++ search.py
@@ -5,28 +5,39 @@ import random
 import constants as c
 import sys
 import pickle
-
-# for i in range(5):
-#     os.system("python3 generate.py")
-#     os.system("python3 simulate.py")
-
-# os.system("python3 simulate.py")
-# hc = HILL_ClIMBER()
-# hc.Evolve()
-# hc.Show_Best()
+from collections import defaultdict
+import matplotlib.pyplot as plt
 
 if len(sys.argv) > 1:
     random.seed(int(sys.argv[1]))
 else:
     random.seed(c.seed)
 
+fitness_curves = defaultdict(list)
+
 for i in range(10):
+    random.seed(i)
     phc = PARALLEL_HILL_ClIMBER()
     phc.Evolve()
-    phc.Show_Best()
+    # phc.Show_Best()
+
     with open(f"phc{i}.pickle", "wb") as f:
         pickle.dump(phc, f)
-    # pickle the phc object
+
+    fitness_curves[f"Random Seed {i}"] = phc.fitnessCurves
+
+phc.Show_Best()
 
 # Plot all 10 runs
+fig, ax = plt.subplots()
+for key, value in fitness_curves.items():
+    ax.plot(value, label=key)
+
+ax.legend()
+ax.set_title("Fitness Curves of Ludobots with Different Random Seeds")
+ax.set_xlabel("Best Fitness")
+ax.set_ylabel("Generation")
+
+plt.show()
+
 # phc.Plot()
